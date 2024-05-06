3f5f77e141501284c66f7e8432711ed1452238e0
avkhlevchuk <avkhlevchuk>
2024-05-06 20:30:12 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Artem Khlevchuk on 06.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
